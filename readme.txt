14ddf59d0ebf1338e30ed2a4e74fb262708f1598
schedulista <schedulista@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-07 01:07:19 +0000

    tagging version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/schedulista-shortcode/tags/2.0@909308 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Plugin Name ===
+
+Contributors: Felix Livni
+Plugin Name: Schedulista Shortcode
+Plugin URI: http://www.schedulista.com?utm_source=wordpress-plugin
+Tags: online scheduling, online scheduling software, schedulista, form, shortcode, scheduling
+Author URI: http://www.schedulista.com?utm_source=wordpress-plugin
+Author: Schedulista
+Requires at least: 2.6
+Tested up to: 3.8.3
+Stable tag: 2.0
+Version: 2.0
+License: GPL
+
+Allows the use of a special short code [schedulista] for adding a "Schedule
+Now" button to your WordPress site.
+
+== Description ==
+
+Allows the use of a special short code [schedulista] for adding a "Schedule
+Now" button to you WordPress site. Everything can be done via the
+WordPress Setting GUI - no file editing at all.
+
+A subscription to the
+[Schedulista](http://www.schedulista.com/?utm_source=wordpress-plugin) online scheduling service is required.
+
+Not sure what Schedulista is or if it will be helpful? Sign up up for a [FREE
+Trial
+Account](https://www.schedulista.com/businesses/sign_up?utm_source=wordpress-plugin) and see for yourself.
+
+After you install the widget, you can copy the shortcode with the parameters
+for your business from the [web settings
+page](https://www.schedulista.com/settings/web) and paste it into your
+WordPress form.
+
+== Changelog ==
+
+= 2.0 =
+* Added support for the embedded widget
+* New "Schedule Now" button 
+
+= 1.0 =
+* Add a "Schedule Now" button
+
---
+++ schedulista.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: Schedulista Shortcode Plugin
+Description: Enables shortcode to embed Schedulista scheduling button or form. Usage: <code>[schedulista type="button" code="salon"]</code>.
+Version: 1.00
+License: GPL
+Author: Schedulista
+Author URI: http://www.schedulista.com
+*/
+
+function createSchedulista($atts, $content = null) {
+	extract(shortcode_atts(array(
+		'type'   => 'button',
+		'code'   => '',
+		'width'     => '500',
+		'height'     => '700',
+    ), $atts));
+
+	if (!$code) {
+
+		$error = "
+		<div style='border: 20px solid red; border-radius: 40px; padding: 40px; margin: 50px 0 70px;'>
+			<h3>Copy & Paste Error</h3>
+			<p style='margin: 0;'>Something is wrong with your Schedulista shortcode.</p>
+		</div>";
+
+		return $error;
+
+	} elseif (strcmp(strtolower($type), 'widget') == 0) {
+	    return createSchedulistaEmbedWidget($code, $width, $height);
+	} else {
+	    return createSchedulistaEmbedButton($code);
+	}
+}
+
+function createSchedulistaEmbedButton($code) {
+    $html =  <<<EOS
+<a href='http://$code.schedulista.com/?utm_source=schedule-now-button&utm_medium=wp&utm_campaign=app'>
+<img title='Schedule an Appointment Online'
+src='http://www.schedulista.com/assets/schedule_button.png' alt='Online
+Scheduling Software'>
+</a>
+EOS;
+    return $html;
+}
+
+function createSchedulistaEmbedWidget($code, $width, $height) {
+    $html = <<<EOS
+<iframe id="schedulista-widget-00"
+src="https://www.schedulista.com/schedule/$code?mode=widget"
+allowtransparency="true" frameborder="0" scrolling="no" width="100%"
+height="900px"></iframe>
+<script id="schedulista-widget-script-00" type="text/javascript"
+src="https://www.schedulista.com/schedule/$code/widget.js"></script>
+EOS;
+    return $html;
+}
+
+add_shortcode('schedulista', 'createSchedulista');
+
+?>
